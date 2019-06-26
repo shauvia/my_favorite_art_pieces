5c94999fc43446cdf2dfa0d1359f2988a70f1c40
shauvia <e.szalwinska@gmail.com>
2019-06-25 21:06:52 -0700

    1. demo how to write a function in pseudo-code; 2. function created in class - createArtEntry; 3.my_own function What art you like

--- script.js
+++ script.js
@@ -8,15 +8,46 @@
 // username = "Stephanie";
 // console.log(username);
 
-var username = prompt("What is your name");
+// var username = prompt("What is your name");
 
-document.write('<p class="one">Hello ' + username + '! Welcome to my page.</p>');
+// document.write('<p class="one">Hello ' + username + '! Welcome to my page.</p>');
 
-var wantsToKnowMore = prompt("Do you want to find your favorite masterpiece");
+// var wantsToKnowMore = prompt("Do you want to find your favorite masterpiece");
 
-if (wantsToKnowMore ==='yes' || wantsToKnowMore ==='okay') {
-  document.write("<p> <a href='#two'> Hooray! Check this one.</a> </p>" );
-} else {
-  document.write("<p>Then get lost.</p>");
+// if (wantsToKnowMore ==='yes' || wantsToKnowMore ==='okay') {
+//   document.write("<p> <a href='#two'> Hooray! Check this one.</a> </p>" );
+// } else {
+//   document.write("<p>Then get lost.</p>");
+// }
+
+function createArtEntry(name, artist, year, location) {
+  var html = "<section>" +"<h3>" + name + '</h3>' +
+  '<img src="milkmaid.jpg">' + '<h5>'+ artist +', '+ year +', '+ location + "</h5>" + "<p>The Milkmaid (Dutch: De Melkmeid or Het Melkmeisje), sometimes called The Kitchen Maid.</p>" + 
+"</section>";
+  return html;
 }
 
+var wantsToAdd = prompt("Do you want to add another masterpiece?");
+
+if (wantsToAdd === "yes") {
+  // prompt the user for the  title, artist, location
+  var uName = prompt("What is the title?");
+  var uArtist = prompt("What is the artist");
+  var uYear = prompt("What is the year");
+  var uLocation = prompt("Whare is it located?");
+
+  //hand the info to the function that builds the html and run the function
+  var html = createArtEntry(uName, uArtist, uYear, uLocation);
+
+  // take the html and give it to document.write
+  document.write(html);
+} else {
+  document.write("Thanks for not adding anything");
+}
+
+//pseudocode
+//inputs: title, artist, location
+//works: create html for a marterpiece entry on the site
+//output: the html
+
+ 
